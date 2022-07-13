6b40cbf68aadb9b6987c298870f7b9607253a1a2
Yeonmi122 <105285030+Yeonmi122@users.noreply.github.com>
2022-07-13 15:31:13 +0900

    Add files via upload

---
+++ htdocs/test3.php
@@ -0,0 +1,148 @@
+<!-- <!DOCTYPE html>
+    <head></heead>
+    <body>
+        <form method = "POST" action = "test3.php">
+            시험 점수 :<input type = "number" name = "score">
+            <input type = "submit" name = "submit">
+        </form>
+    </body>
+</html> -->
+<?php
+    //123p 3-1
+    // $s = $_POST["score"];
+    // if(90<=$s and $s<=100){
+    //     echo "수";
+    // }
+    // elseif(80<=$s and $s<=89){
+    //     echo "우";
+    // }
+    // elseif(70<=$s and $s<=79){
+    //     echo "미";
+    // }
+    // elseif(60<=$s and $s <=69){
+    //     echo "양";
+    // }
+    // elseif(0<=$s and $s<=59){
+    //     echo "가";
+    // }
+    // else{
+    //     echo "입력 점수 오류!";
+    // }
+    // 3-2
+    // $sum = 0;
+    // $i = 100;
+    // while($i <= 200){
+    //     if($i%2 !=0)
+    //         $sum += $i;
+    //         $i++;
+    // }
+    //     echo $sum;
+
+    //3-3
+    // $sum = 0;
+    // for ($i = 100; $i <= 200; $i++){
+    //         if($i%2 !=0)
+    //             $sum += $i;
+    // }
+    // echo $sum;
+
+    //3-4
+    // $i = 1;
+    // while($i<=100){
+    //     if($i%3 == 0){
+    //         if($i%10==0) echo $i."<br>";
+    //         else echo $i." ";
+            
+    //     }
+    //     $i++;
+    // }
+
+    // $n = 1;
+    // $count = 0;
+    // while ($n<=100) {
+    //     if ($n%3 == 0)  {
+    //         echo "$n ";
+    //         $count++;
+
+    //         if ($count%10 == 0)
+    //             echo "<br>";
+    //     }
+    //     $n++;
+    // }
+
+    //3-5
+    // $count = 0;
+    // for($i=300; $i<=350; $i++){
+    //     if($i%5!=0){
+    //         echo "$i ";
+    //         $count++;
+    //         if($count%10 == 0)
+    //         echo "<br>";
+    //     }
+    // }
+
+    //3-6
+    // echo "-------------------<br>";
+    // echo "인치 센티미터<br>";
+    // echo "-------------------<br>";
+    // $i = 10;
+    // while($i<=20){
+    //     $s = $i*2.54;
+    //     echo " $i $s<br>";
+    //     $i+=2;
+    // }
+    // echo "-------------------<br>";
+
+    // //3-7
+    // echo "-------------------<br>";
+    // echo "인치 센티미터<br>";
+    // echo "-------------------<br>";
+    // for($i = 10; $i <=20; $i+=2){
+    //     $s = $i*2.54;
+    //     echo "$i $s<br>";
+    // }
+    // echo "-------------------<br>";
+
+    // //3-8
+    // echo "--------------------------<br>";
+    // echo "달러 원화 유로<br>";
+    // echo "--------------------------<br>";
+    // for($i = 10; $i <=20; $i+=2){
+    //     $w = $i*1180.60;
+    //     $u = $i*1350.30;
+    //     echo "$i $w $u<br>";
+    // }
+    // echo "--------------------------<br>";
+
+    // //3-9
+    // $string = "";
+
+    // $string.="<table border ='1'>";
+    // $string.="<tr><th width = '100'>달러</th><th width = '100'>원화</th><th width = '100'>유로</th>";
+    
+    // for($i = 10; $i <=20; $i+=2){
+    //     $w = $i*1180.60;
+    //     $u = $i*1350.30;
+    //     $string.="<tr><th>$i</th><th>$w</th><th>$u</th>";
+    // }
+    // $string.="</table>";
+
+    // echo $string;
+
+    // //3-10
+    // for($i=1; $i<=5;$i++){
+    //     for($j=1; $j<=10; $j++){
+    //         echo "* ";
+    //     }
+    //     echo "<br>";
+    // }
+
+    // //3-11
+    // for($i=1; $i<=10; $i++){
+    //     for($j=1; $j<=$i; $j++){
+    //         echo "* ";
+    //     }
+    //     echo "<br>";
+    // }
+
+?>
