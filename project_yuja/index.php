bb30ae2b3f78e646e65b438d7372f9af201e449d
Yeonmi122 <105285030+Yeonmi122@users.noreply.github.com>
2022-07-26 16:24:55 +0900

    Add files via upload

--- project_yuja/index.php
+++ project_yuja/index.php
@@ -1,14 +1,3 @@
-<?php
-    include"../include/header.php";
-    includde"../include/main.php";
-?>
-
-<?php
-    session_start();
-
-    if(issue($_SESSION["userid"]))
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,24 +5,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>유트브 자막 제작 구인 사이트</title>
-    <link rel = "stylesheet" href = "index.css">
+    <link rel = "stylesheet" href = "main.css">
+    <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <header>
-        <h3 class = "logo">
-            <a href = "index.php">유트브 자막 제작 구인 사이트</a>
-        </h3>
-        <ul class = "top menu">
-
-        <?php
-          if($userid){
-            echo"<li>홈페이지에 오신걸 환영합니다.</li>";
-          }
-          else{
-            $logged = $username."
-          }
-        ?>  
-    </header>
-
+    <div class=".container-fluid">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class = "logo">
+                <a href="#"><img src = "./img/logo_1.png" width = "60%"></a>
+            </div>
+            <div class = "main_title">
+                <h3>유트브 자막 제작 구인 사이트</h3>
+            </div>
+    
+            <div class="col-md-3 text-end">
+              <button type="button" class="btn btn-outline-primary me-2" id="login">Login</button>
+              <button type="button" class="btn btn-primary" id="sign-up">Sign-up</button>
+            </div>
+        </header>
+    </div>
+    <div class = "main_img">
+        <img src = "./img/home_1.png" width = 70% height="400px">
+    </div>
+    <div class="row mb-3 text-center">
+      <div class="col-6 themed-grid-col" id = "ko">한국어 자막</div>
+      <div class="col-6 themed-grid-col" id = "gl">외국어 자막</div>
+    </div>
+    <div class="row mb-3 text-center">
+      <div class="col-6 themed-grid-col">자유게시판</div>
+      <div class="col-6 themed-grid-col">공지사항</div>
+    </div>
 </body>
-<footer></footer>
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+
+    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+    </a>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+  </footer>
+</html>
---
+++ project_yuja/page2.php
@@ -0,0 +1,2 @@
+<?php include $_SERVER['DOCUMENT_ROOT']."/PROJECT_YUJA/index.php"?>
+
