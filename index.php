fd35ce8c44edcb467e27a3beb3f75a46d6f07c7d
Yeonmi122 <105285030+Yeonmi122@users.noreply.github.com>
2022-08-17 15:52:36 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,55 +1,51 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>유트브 자막 제작 구인 사이트</title>
-    <link rel = "stylesheet" href = "main.css">
-    <link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Jua&family=Kiwi+Maru:wght@300&family=Kosugi+Maru&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/p3/garam_team/main/index.css">
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1&display=swap" rel="stylesheet">
+    <title>main</title>
 </head>
 <body>
-    <div class=".container-fluid">
-        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-            <div class = "logo">
-                <a href="#"><img src = "./img/logo_2.png" width = "60%"></a>
-            </div>
-            <div class = "main_title">
-                <h3>유트브 자막 제작 구인 사이트</h3>
-            </div>
     
-            <div class="col-md-3 text-end">
-              <button onclick = "location.href = login.php" type="button" class="btn btn-outline-primary me-2" id="login">Login</button>
-              <button onclick = "location.href = sign_tp.php" type="button" class="btn btn-primary" id="sign-up">Sign-up</button>
+    <header>
+        <div class = "main_1">
+            <div class = "info" width = 50% height = 50%>
+                <div class = info_img>
+                    <img src="/p3/garam_team//img/osorosiki_white.png" width="60%" height="60%" >
+                </div>
+                <div class = info_w>
+                    <h3>
+                        ホラーマニア向けサイトで、怖い場所に直接行くことができ、<br>
+                        同じ趣味を持つ人と趣味を共有することができます。
+                    </h3>
+                </div>
             </div>
-        </header>
-    </div>
-  <main>
-    <div class = "main_img">
-        <img src = "./img/main_5.png" width = 80% height="400px">
-    </div>
-    <div class="row mb-3 text-center">
-      <div class="col-6 themed-grid-col" id = "ko">한국어 자막</div>
-      <div class="col-6 themed-grid-col" id = "gl">외국어 자막</div>
-    </div>
-    <div class="row mb-3 text-center">
-      <div class="col-6 themed-grid-col">자유게시판</div>
-      <div class="col-6 themed-grid-col">공지사항</div>
-    </div>
-  </main>
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
-
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-    </a>
-
-    <div class="nav col-md-4 justify-content-end">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">(주)YOUJA</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">고객센터: 1544-1234(평일 오전10:00~ 오후 16:00)</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">yooyeonmi122@gmail.com</a></li>
-    </div>
-  </footer>
+            <div class ="map">
+                <a href="#"><img src = "/p3/garam_team//img/map.png" width = "100%" height="100%" id = map_i></a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class = board_1 >
+            <div class = "board1" >
+                <a href="#"><img src = "/p3/garam_team//img/review.png" width ="90%" height="90%" id="review"></a>
+            </div>
+            <div class = "board2" >
+                <a href="#"><img src = "/p3/garam_team//img/free.png" width ="90%" height="90%" id="free"></a>
+            </div>
+        </div>
+        <div class = board_2 >
+            <div class = "board3">
+                <a href="#"><img src = "/p3/garam_team//img/art.png" width ="90%" height="90%" id="art"></a>
+            </div>
+            <div class = "board4">
+                <a href="#"><img src = "/p3/garam_team//img/story.png" width ="90%" height="90%" id="story"></a>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
